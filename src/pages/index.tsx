eb13668eea87177a4551ad66f0e383671029db29
ja153903 <jaimeabbariao@gmail.com>
2022-07-31 14:05:36 -0400

    feat: fixing file structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <div>Home Page</div>;
+  return <div className="text-xl font-bold">Home Page</div>;
 };
 
 export default Home;
